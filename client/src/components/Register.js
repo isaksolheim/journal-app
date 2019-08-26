4ac58261203386e5c995459ee7b6af76704f823a
wickdeeb <isak.m.h.s@gmail.com>
2019-08-26 14:03:07 +0200

    Add register functionality

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,18 +1,53 @@
 import React from 'react';
+import axios from 'axios';
 
-function Register() {
-  return(
-    <form>
-      <label>Name</label>
-      <input type="text" />
-      <label>Email</label>
-      <input type="text" />
-      <label>Password</label>
-      <input type="password" />
-      <label>Repeat Password</label>
-      <input type="password" />
-    </form>
-  );
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+  }
+
+  inputHandler = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    axios.post('http://localhost:5000/api/users/register', newUser);
+    
+    console.log('User created!');
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.submitHandler}>
+        <label>Name</label>
+        <input id="name" type="text" onChange={this.inputHandler} />
+        <label>Email</label>
+        <input id="email" type="text" onChange={this.inputHandler} />
+        <label>Password</label>
+        <input id="password" type="password" onChange={this.inputHandler} />
+        <label>Repeat Password</label>
+        <input id="password2" type="password" onChange={this.inputHandler} />
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
 }
 
 export default Register;
